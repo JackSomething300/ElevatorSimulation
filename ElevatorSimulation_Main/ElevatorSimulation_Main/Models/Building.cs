3c1f0838430b246c00207f6f99259b7606776a3b
martindotslash <martin@dotslash.co.za>
2024-07-17 17:23:24 +0200

    Initial project setup and adding models

---
+++ ElevatorSimulation_Main/ElevatorSimulation_Main/Models/Building.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorSimulation_Main.Models
+{
+    public class Building
+    {
+        public int NumberOfFloors { get; set; }
+        public List<Elevator> Elevators { get; set; }
+    }
+}
